a23b427796ae4362a64d6199cf8bcd5fc7fcc66f
Sung Hyouk Choi <sakebite@snu.ac.kr>
2021-09-17 09:35:40 +0900

    new ruby version correction

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to my repository",
+        "excerpt":"1st content   최초작성   This is first state of my repository: test project  GitHub에서 작성한 최초의 글입니다.   2021-08-25 Wed   sidebar 만들고 수정   2021-09-09 Thr  ","categories": ["justchat"],
+        "tags": ["Just_chat","Blog","GitHub"],
+        "url": "/justchat/welcome/",
+        "teaser": null
+      },{
+        "title": "Mathematics for machine learning - Chapter 1: Introduction and motivation",
+        "excerpt":"머신러닝의 수학적 기초를 배운다는 것은 새 머신러닝 해법을 창조하고, 기존 접근법을 이해하고 오류를 해결하며, 방법론의 가정과 제약을 배우는 것을 촉진한다. - 그래서 중요하다. 1.1 Finding words for intuitions Summary We represent data as vectors. We choose an appropriate model, either using the probabilistic or optimization view. We learn from available...","categories": ["MATH"],
+        "tags": ["mathematics","introduction","MML-book"],
+        "url": "/math/chapter_1/",
+        "teaser": null
+      }]
