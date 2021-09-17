5eec8f2e46f60e7369b128ea9c71d952ba59383f
Sung Hyouk Choi <sakebite@snu.ac.kr>
2021-09-17 17:42:36 +0900

    blog change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": ["mathematics","introduction","MML-book"],
         "url": "/math/chapter_1/",
         "teaser": null
+      },{
+        "title": "K-means algorithm의 간단한 원리",
+        "excerpt":"출처: 수학과 함께하는 AI 기초 K-Means 군집화 알고리즘의 동작 과정 알아보기 [1단계] 표본 공간에 K개의 중심을 무작위로 생성하기 군집 (비슷한 특성을 지닌 데이터들의 집합)을 만들기 위해서 중심 (centroid)이 필요한데, 이유는 중심을 기준으로 가까운 거리의 점을 같은 군집에 포함할지 말지를 결정하기 때문 중심은 무작위로 생성하도록 한다. [2단계] 각 표본을 가장 가까운...","categories": ["ML"],
+        "tags": ["ML","algorithm"],
+        "url": "/ml/kmeans/",
+        "teaser": null
       }]
